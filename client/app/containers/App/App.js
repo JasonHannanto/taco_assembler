506c5154134aed1a527ec95fbf9662584b29e60e
Jason Hannanto <hannanto@usc.edu>
2018-09-17 02:15:43 -0700

    Remove Header

--- client/app/containers/App/App.js
+++ client/app/containers/App/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
-import Header from '../../Components/Header/Header';
-
 const App = ({ children }) => (
   <>
-    <Header />
     <main>
       {children}
     </main>
